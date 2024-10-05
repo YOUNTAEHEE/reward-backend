9cd0ec687314fb5a2c8273554634aa9d36c7f06c
YOUNTAEHEE <f_hyang@naver.com>
2024-10-05 23:41:58 +0900

    fix:env

--- src/main/java/com/yoon/reward/RewardApplication.java
+++ src/main/java/com/yoon/reward/RewardApplication.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class RewardApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(RewardApplication.class, args);
 	}
 
