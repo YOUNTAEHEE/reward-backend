35c5be9d2d0f45cf9870f03ae396894a6323ecf9
YOUNTAEHEE <f_hyang@naver.com>
2024-10-25 15:09:09 +0900

    chore:README 수정

--- src/main/java/com/yoon/reward/point/query/controller/PointTransactionController.java
+++ src/main/java/com/yoon/reward/point/query/controller/PointTransactionController.java
@@ -4,10 +4,7 @@ import com.yoon.reward.point.command.application.dto.PointDetailDTO;
 import com.yoon.reward.point.query.service.PointTransactionService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,9 +17,10 @@ public class PointTransactionController {
     public PointTransactionController(PointTransactionService pointTransactionService) {
         this.pointTransactionService = pointTransactionService;
     }
-    //입출금 내역 조회
+    //입출금 내역 조회(생각해보니까 이거 리스트로 반환아님?)
     @GetMapping("/point/detail")
-    public ResponseEntity<PointDetailDTO> pointTransactionDetail(String userId){
+    public ResponseEntity<PointDetailDTO> pointTransactionDetail(@RequestBody Map<String, String> requestBody){
+        String userId = requestBody.get("userId");
         try{
             PointDetailDTO pointDetailDTO = pointTransactionService.pointTransactionDetail(userId);
             return ResponseEntity.ok(pointDetailDTO);
@@ -31,14 +29,15 @@ public class PointTransactionController {
         }
     }
     //유저 테이블 총 포인트 조회
-    @GetMapping()
-    public ResponseEntity<Map<String, Object>> getTotalUserPoint(@RequestParam String userId){
+    @PostMapping("/point")
+    public ResponseEntity<?> getTotalUserPoint(@RequestBody Map<String, String> requestBody){
+        String userId = requestBody.get("userId");
         try{
             Map<String, Object> userPointInfo = pointTransactionService.getTotalUserPoint(userId);
             return ResponseEntity.ok(userPointInfo);
         } catch(IllegalArgumentException e){
             Map<String, Object> errorResponse = new HashMap<>();
-            errorResponse.put("error", "유저 정보를 찾을 수 없습니다.");
+            errorResponse.put("message", "유저 정보를 찾을 수 없습니다.");
             return ResponseEntity.badRequest().body(errorResponse);
         }
 
